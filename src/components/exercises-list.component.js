ed063f2fbe1fb96782763511b1a0994edd3d18e0
phariwud paprakob <priteomg@gmail.com>
2020-08-17 18:41:35 +0700

    show alert before del

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -73,31 +73,31 @@ export default function ExercisesList(props) {
     setOpen(false);
   };
 
-  const DialogDel = () => {
-    return (
-      <Dialog
-        fullScreen={fullScreen}
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="responsive-dialog-title"
-      >
-        <DialogTitle id="responsive-dialog-title">
-          {"Use Google's location service?"}
-        </DialogTitle>
-        <DialogContent>
-          <Typography>Are you sure to delete this ? {}</Typography>
-        </DialogContent>
-        <DialogActions>
-          <Button autoFocus onClick={handleClose} color="primary">
-            Disagree
-          </Button>
-          <Button onClick={handleClose} color="primary" autoFocus>
-            Agree
-          </Button>
-        </DialogActions>
-      </Dialog>
-    );
-  };
+  // const DialogDel = () => {
+  //   return (
+  //     <Dialog
+  //       fullScreen={fullScreen}
+  //       open={open}
+  //       onClose={handleClose}
+  //       aria-labelledby="responsive-dialog-title"
+  //     >
+  //       <DialogTitle id="responsive-dialog-title">
+  //         {"Use Google's location service?"}
+  //       </DialogTitle>
+  //       <DialogContent>
+  //         <Typography>Are you sure to delete this ? {}</Typography>
+  //       </DialogContent>
+  //       <DialogActions>
+  //         <Button autoFocus onClick={handleClose} color="primary">
+  //           Disagree
+  //         </Button>
+  //         <Button onClick={handleClose} color="primary" autoFocus>
+  //           Agree
+  //         </Button>
+  //       </DialogActions>
+  //     </Dialog>
+  //   );
+  // };
 
   const deleteExercise = (id) => {
     if (window.confirm("Are you sure")) {
